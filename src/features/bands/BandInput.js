81c3e32ebf130e1c065bf47760858143ab364e2f
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-02-24 16:18:21 -0800

    done

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from 'react'
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({ onBandSubmit }) {
+  const [name, setName] = useState(' ')
+  
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onBandSubmit(name)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name 
+        <input type="text" 
+        name="name" 
+        value={name} 
+        onChange={handleChange} 
+        />
+      </label>
+      <button type="submit">Add Band</button>
+    </form>
+  )
 }
 
 export default BandInput;
--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import BandInput from './BandInput' //write the BandInput component as a child of BandsController
+import { bandAdded } from './bandsSlice'
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
-}
+  const bands = useSelector((state) => state.bands.entities)
+  const dispatch = useDispatch() //Dispatched action required to initiate this update should be set up in BandsContainer and passed down as a prop onBandSubmit to BandInput
 
+  function handleBandSubmit(name) {
+    dispatch(bandAdded(name)) /* The dispatched action required to initiate this update is set up and passed down as the prop onBandSubmit to BandInput */
+  }
+ 
+ return (
+   <div>
+     <BandInput onBandSubmit={handleBandSubmit} /> 
+     <ul>
+       {bands.map((band, i) => (
+         <li key={i}> {band} </li>
+       ))}
+     </ul>
+   </div>
+ )
+}
+ 
 export default BandsContainer;
